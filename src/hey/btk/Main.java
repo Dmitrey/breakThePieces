e30a4b7163bdbe93423251863f31deb1da9b498d
Dmitrey <dmitrijp8@gmail.com>
2022-04-03 22:44:33 +0300

    something works

--- src/hey/btk/Main.java
+++ src/hey/btk/Main.java
@@ -1,21 +1,22 @@
 package hey.btk;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
 public class Main {
 
     public static void main(String[] args) {
-//        String shape = String.join("\n", new String[]
-//                {"+------------+",
-//                        "|            |",
-//                        "|            |",
-//                        "|            |",
-//                        "+------+-----+",
-//                        "|      |     |",
-//                        "|      |     |",
-//                        "+------+-----+"});
+        String shape = String.join("\n", new String[]
+                {       "+--------+",
+                        "|        |",
+                        "|        |",
+                        "|        |",
+                        "+--+-----+",
+                        "|  |     |",
+                        "|  |     |",
+                        "+--+-----+"});
         String shape2 = String.join("\n", new String[]
                 {
                         "   +----+-------+",
@@ -30,12 +31,11 @@ public class Main {
                 });
 
 
-        System.out.println(shape2);
-        for (String str : process(shape2)) {
+        System.out.println(shape);
+        for (String str : process(shape)) {
             System.out.println(str);
         }
     }
-
 
     public static String[] process(String shape) {
         String[] init = shape.split("\n");
@@ -43,43 +43,81 @@ public class Main {
         for (int i = 0; i < matrix.length; i++) {
             matrix[i] = init[i].split("");
         }
-
         int[] start = {0, 3};
 
+        DirectionHolder holder = new DirectionHolder(matrix, start);
         while (true) {
-            step(start);
+            holder.stepForward();
+            if (Arrays.equals(holder.getPoint(), start)){
+                break;
+            }
+            if (holder.canGoRight()){
+                holder.turnRight();
+            }else {
+                while (!holder.canGoForward()){
+                    holder.turnRight();
+                }
+            }
+        }
+        return null;
+    }
+}
+
+class DirectionHolder {
+    private int index = 0;
+    private int[] point;
+    private final String[][] matrix;
+    private final List<Function<int[], int[]>> list = List.of(
+            (int[] arr) -> new int[]{arr[0], arr[1] + 1},
+            (int[] arr) -> new int[]{arr[0] + 1, arr[1]},
+            (int[] arr) -> new int[]{arr[0], arr[1] - 1},
+            (int[] arr) -> new int[]{arr[0] - 1, arr[1]}
+    );
+    private Function<int[], int[]> currentDirection = list.get(index);
+    private Function<int[], int[]> rightDirection;
+
+    public DirectionHolder(String[][] matrix, int[] start) {
+        this.matrix = matrix;
+        point = start;
+        updateDirections();
+    }
+
+    void updateDirections() {
+        currentDirection = list.get(index);
+        try {
+            rightDirection = list.get(index + 1);
+        } catch (Exception e) {
+            rightDirection = list.get(0);
         }
     }
 
-
-    static void step(int[] start) {
-        int[] point = start;
-        point = DirectionHolder.currentDirection.apply(point);
+    public void stepForward() {
+        point = currentDirection.apply(point);
+        show();
     }
 
-    static boolean checkRight(int[]) {
-
+    public boolean canGoRight() {
+        int[] checkPoint = rightDirection.apply(point);
+        String element = matrix[checkPoint[0]][checkPoint[1]];
+        return element.equals("-") || element.equals("|") || element.equals("+");
     }
 
-    static void changeDirection() {
-
+    public boolean canGoForward() {
+        int[] checkPoint = currentDirection.apply(point);
+        String element = matrix[checkPoint[0]][checkPoint[1]];
+        return element.equals("-") || element.equals("|") || element.equals("+");
     }
 
+    public void turnRight() {
+        index = index < 3 ? index + 1 : 0;
+        updateDirections();
+    }
 
-    static class DirectionHolder {
-        static int index = 0;
-        static List<Function<int[], int[]>> list = List.of(
-                (int[] arr) -> new int[]{arr[0], arr[1] + 1},
-                (int[] arr) -> new int[]{arr[0] + 1, arr[1]},
-                (int[] arr) -> new int[]{arr[0], arr[1] - 1},
-                (int[] arr) -> new int[]{arr[0] - 1, arr[1]}
-        );
-        Function<int[], int[]> currentDirection = list.get(index);
+    public void show(){
+        System.out.print(matrix[point[0]][point[1]]);
+    }
 
-        static void turnRight() {
-            index = index < 4 ? index + 1 : 0;
-        }
-
-        static void stepForward(){}
+    public int[] getPoint() {
+        return point;
     }
 }
